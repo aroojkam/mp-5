120eaa25464539665eea695fc1a5faf65e3833eb
arooj <aroojkam7@gmail.com>
2025-04-18 14:12:01 -0400

    mp51

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,17 +1,19 @@
 import getCollection from '@/db'
 import { redirect } from 'next/navigation'
 
-export default async function Page(props: { params: { alias: string } }) {
-  // Access the params from props and await it
-  const paramsObj = await props.params
-  const alias = paramsObj.alias
-  
+interface PageProps {
+  params: {
+    alias: string
+  }
+}
+
+export default async function Page({ params }: PageProps) {
   const urls = await getCollection('urls')
-  const record = await urls.findOne({ alias })
-  
-  if (record) {
+  const record = await urls.findOne({ alias: params.alias })
+
+  if (record?.url) {
     redirect(record.url)
   }
-  
-  return <div className="text-center mt-10">Alias not found.</div>
+
+  return <div className="text-center mt-10 text-red-500">Alias not found.</div>
 }
